2329c36c166a0c9c965f6c7f58aed109ee5b780f
odoo <kem1101231>
2018-10-24 02:24:58 -0400

    update

--- Project_SMO_Inventory/static/src/path_config.py
+++ Project_SMO_Inventory/static/src/path_config.py
@@ -4,8 +4,10 @@ import sys
 
 dirreq = dirname(dirname(abspath(__file__)))
 
-#config python file path for Ubuntu machines
-sys.path.append('{}/Project_SMO_Inventory/static/src'.format(dirreq))
+class PathConfig:
+        def __init__(self):
+            #config python file path for Ubuntu machines
+            sys.path.append('{}/Project_SMO_Inventory/static/src'.format(dirreq))
 
-#configure python file path for Windows machines
-sys.path.append('{}\Project_SMO_Inventory\static\src'.format(dirreq))
+            #configure python file path for Windows machines
+            sys.path.append('{}\Project_SMO_Inventory\static\src'.format(dirreq))
